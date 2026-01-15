de444f2c41d5ffbb77671feba388759689192b52
Ramon van Hinthem <ramon@vanhinthem.nl>
2026-01-15 09:12:14 +0100

    Add token provider to ServiceClient and create solution file

--- DataverseDebugger.Runner/Services/Hybrid/ServiceClientOrganizationServiceFactory.cs
+++ DataverseDebugger.Runner/Services/Hybrid/ServiceClientOrganizationServiceFactory.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading.Tasks;
 using DataverseDebugger.Runner.Abstractions;
 using Microsoft.PowerPlatform.Dataverse.Client;
 using Microsoft.Xrm.Sdk;
@@ -7,18 +8,32 @@ namespace DataverseDebugger.Runner.Services.Hybrid
 {
     internal sealed class ServiceClientOrganizationServiceFactory : ILiveOrganizationServiceFactory
     {
-        public IOrganizationService? CreateLiveService(string? orgUrl, string? accessToken, out IDisposable? disposable)
+public IOrganizationService? CreateLiveService(string? orgUrl, string? accessToken, out IDisposable? disposable)
         {
             disposable = null;
+
             if (string.IsNullOrWhiteSpace(orgUrl) || string.IsNullOrWhiteSpace(accessToken))
+            {
+                return null;
+            }
+
+            Uri instanceUri;
+            if (!Uri.TryCreate(orgUrl, UriKind.Absolute, out instanceUri))
             {
                 return null;
             }
 
             try
             {
-                var connectionString = $"AuthType=OAuth;Url={orgUrl};AccessToken={accessToken};";
-                var client = new ServiceClient(connectionString);
+                // ServiceClient will call this delegate whenever it needs an access token.
+                // No MSAL flows are invoked, so there is no interactive prompt.
+                Func<string, Task<string>> tokenProvider = (string instance) =>
+                {
+                    return Task.FromResult(accessToken);
+                };
+
+                ServiceClient client = new ServiceClient(instanceUri, tokenProvider, useUniqueInstance: true);
+
                 disposable = client;
                 return client;
             }
